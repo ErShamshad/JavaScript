6376a74a9f3659dc1ca9c9dfcc81cd626e813772
Md Shamshad Ansari <mdshamshad.ansari@ad.infosys.com>
2022-05-16 23:13:14 +0530

    Added Math Object File in javaScript.

---
+++ Math_Object.js
@@ -0,0 +1,22 @@
+
+let x=5;
+let y=3;
+let z=x+y;
+z=x-y;
+z=x*y;
+z=x/y;
+z=Math;
+z=Math.PI;
+z=Math.E;
+z=Math.round(5.5);
+z=Math.ceil(5.2);
+z=Math.floor(5.8);
+z=Math.abs(-5.8);
+z=Math.sqrt(64)
+z=Math.pow(3,2);
+z=Math.min(2,4,-9);
+z=Math.max(3,8,9);
+z=Math.random();
+z=100*Math.random();
+z=50+(50*Math.random());
+console.log(z);
